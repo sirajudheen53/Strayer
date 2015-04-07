cfa5505a116c35f563f703750383976de6e57a04
Sirajudheen <siraju.ka@gmail.com>
2015-04-07 13:54:27 +0530

    Added side menu view controller

---
+++ Strayer/ViewControllers/SideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewController.h
+//  Strayer
+//
+//  Created by user on 07/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SideMenuViewController : NSObject
+
+@end
---
+++ Strayer/ViewControllers/SideMenuViewController.m
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewController.m
+//  Strayer
+//
+//  Created by user on 07/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#import "SideMenuViewController.h"
+
+@implementation SideMenuViewController
+
+@end
