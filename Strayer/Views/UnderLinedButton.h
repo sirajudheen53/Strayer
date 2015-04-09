61656cc93775e6f477699fd97a0f5b0f5a280ab1
Sirajudheen <siraju.ka@gmail.com>
2015-04-09 17:32:37 +0530

    Added under lined button

---
+++ Strayer/Views/UnderLinedButton.h
@@ -0,0 +1,13 @@
+//
+//  UnderLinedButton.h
+//  Strayer
+//
+//  Created by user on 09/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnderLinedButton : UIButton
+
+@end
---
+++ Strayer/Views/UnderLinedButton.m
@@ -0,0 +1,21 @@
+//
+//  UnderLinedButton.m
+//  Strayer
+//
+//  Created by user on 09/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#import "UnderLinedButton.h"
+
+@implementation UnderLinedButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
