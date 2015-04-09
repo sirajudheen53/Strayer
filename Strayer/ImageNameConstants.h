6ceb3431211e437291b940ce41234be775acc782
Sirajudheen <siraju.ka@gmail.com>
2015-04-09 11:58:40 +0530

    navigation bar images added

---
+++ Strayer/ImageNameConstants.h
@@ -0,0 +1,10 @@
+//
+//  ImageNameConstants.h
+//  Strayer
+//
+//  Created by user on 09/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#define NAVIGATION_BAR_BACKGROUND_IMAGE @"navigationBarBackground"
+#define NAVIGATION_BAR_LEFT_MENU_IMAGE @"leftMenuBackground"
