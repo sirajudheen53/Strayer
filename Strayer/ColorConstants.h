603de1ee0e0119edbc4ee9af0d49b661c59ca988
Sirajudheen <siraju.ka@gmail.com>
2015-04-09 17:31:29 +0530

    Added color constants files

---
+++ Strayer/ColorConstants.h
@@ -0,0 +1,11 @@
+//
+//  ColorConstants.h
+//  Strayer
+//
+//  Created by user on 09/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#define LOGIN_INPUT_BOX_BORDER_COLOR [UIColor colorWithRed:127/255.0 green:127/255.0 blue:127/255.0 alpha:1]
+
+#define LOGIN_INPUT_BOX_PLACE_HOLDER_COLOR [UIColor colorWithRed:90/255.0 green:80/255.0 blue:108/255.0 alpha:1]
