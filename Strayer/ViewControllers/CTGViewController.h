bb1eb74ff16c19a4829d7597881caf1aaff46998
Sirajudheen <siraju.ka@gmail.com>
2015-04-09 11:56:40 +0530

    CTGViewController added

---
+++ Strayer/ViewControllers/CTGViewController.h
@@ -0,0 +1,17 @@
+//
+//  CTGViewController.h
+//  Strayer
+//
+//  Created by user on 09/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PKRevealController.h"
+
+@interface CTGViewController : UIViewController
+
+
+@property (nonatomic, strong, readwrite) PKRevealController *revealController;
+
+@end
