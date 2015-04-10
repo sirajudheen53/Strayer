0a303859354dd0c4b6791c231b83517557b04e92
Sirajudheen <siraju.ka@gmail.com>
2015-04-10 12:28:26 +0530

    constants added

--- Strayer/Constants.h
+++ Strayer/Constants.h
@@ -6,7 +6,15 @@
 //  Copyright (c) 2015 mobomo. All rights reserved.
 //
 
+@class CTGAppDelegate;
 
 #define DAY_IN_SECONDS 60*60*24
 
 #define kAuthToken @"_authToken"
+#define kUserLoggedIn @"_userLoggedIn"
+
+#define MAXIMUM_VACCATION_DAYS 30
+
+#define APP_DELEGATE (CTGAppDelegate *)[[UIApplication sharedApplication] delegate]
+
+#define WEBSERVICE_RESPONSE_DATE_FORMAT @"%Y-%m-%d %H:%M:%S"
---
+++ Strayer/UserDefaultConstants.h
@@ -0,0 +1,9 @@
+//
+//  UserDefaultConstants.h
+//  Strayer
+//
+//  Created by user on 10/04/15.
+//  Copyright (c) 2015 mobomo. All rights reserved.
+//
+
+#define USER_SETTINGS_USER_DEFAULT_KEY @"userSettingsUserDefaultsKey"
