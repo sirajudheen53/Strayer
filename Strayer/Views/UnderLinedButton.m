b20b1149ce9e8c42b42bb2e00e596c9fe7fe32c0
Sirajudheen <siraju.ka@gmail.com>
2015-04-10 09:52:54 +0530

    Added under lined button

--- Strayer/Views/UnderLinedButton.m
+++ Strayer/Views/UnderLinedButton.m
@@ -10,12 +10,24 @@
 
 @implementation UnderLinedButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void) drawRect:(CGRect)rect {
+    CGRect textRect = self.titleLabel.frame;
+    
+    // need to put the line at top of descenders (negative value)
+    CGFloat descender = self.titleLabel.font.descender;
+    
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    
+    // set to same colour as text
+    CGContextSetStrokeColorWithColor(contextRef, self.titleLabel.textColor.CGColor);
+    
+    CGContextMoveToPoint(contextRef, textRect.origin.x, textRect.origin.y + textRect.size.height);
+    
+    CGContextAddLineToPoint(contextRef, textRect.origin.x + textRect.size.width, textRect.origin.y + textRect.size.height);
+    
+    CGContextClosePath(contextRef);
+    
+    CGContextDrawPath(contextRef, kCGPathStroke);
 }
-*/
 
 @end
